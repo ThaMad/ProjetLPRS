57f02c3df6b70a7fbf462e2e2eb47c33485d1190
ThaMad <thanudi.m@gmail.com>
2021-11-16 08:46:32 +0100

    Correction balise chatbox
    fermeture form

--- view/chatbox/chatbox.php
+++ view/chatbox/chatbox.php
@@ -241,7 +241,7 @@ $user = $user->fetchall();
                 <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt="">
                 <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt="">
                 <input type="submit" class="btn btn-secondary">Envoyer</input>
-                <form>
+                </form>
         </footer>
             </main>
         </div>
