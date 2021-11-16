ab38b15e50a362dc569cb2f3c592ee802d871696
ThaMad <thanudi.m@gmail.com>
2021-11-16 08:45:00 +0100

    Correction button -> input
    Input à la place de button dans chatbox.php car button ne fonctionnait pas

--- view/chatbox/chatbox.php
+++ view/chatbox/chatbox.php
@@ -166,7 +166,7 @@ $user = $user->fetchall();
                         <textarea name="message" placeholder="Commencez la conversation, envoyez le premier message !"> </textarea>
                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt="">
                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt="">
-                        <input type="submit" class="btn btn-secondary" value="test">
+                        <input type="submit" class="btn btn-secondary" value="Envoyer">
                         <form>
                 </footer>
             </main>
@@ -240,7 +240,7 @@ $user = $user->fetchall();
                 <textarea name="message" placeholder="Saisissez votre message"> </textarea>
                 <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_picture.png" alt="">
                 <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1940306/ico_file.png" alt="">
-                <button type="submit" class="btn btn-secondary" onclick="$('#testForm').submit()">test</button>
+                <input type="submit" class="btn btn-secondary">Envoyer</input>
                 <form>
         </footer>
             </main>
